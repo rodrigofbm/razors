e4fdb836b0820bcf84654266d44da9fcb89700c3
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-06-12 16:16:16 -0300

    handle sidebar

--- src/components/navbar/navbar.svelte
+++ src/components/navbar/navbar.svelte
@@ -1,8 +1,11 @@
 <script>
   import SmallNavbar from "./small-navbar.svelte";
   import BigNavbar from "./big-navbar.svelte";
+  import globalStore from "../../stores/global-store";
 
   let screenWidth;
+
+  $: if (screenWidth > 992) globalStore.toggleItem("sidebar", false);
 </script>
 
 <svelte:window bind:innerWidth={screenWidth} />
