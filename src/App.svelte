a183600cfb34d4623d19a282f46d89b62a1bae36
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-06-12 12:23:19 -0300

    project structured

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  // router
+  import { Router, Route, Link } from "svelte-routing";
+  // pages
+  import Home from "./pages/home.svelte";
+  import Login from "./pages/login.svelte";
+  import Checkout from "./pages/checkout.svelte";
+  import About from "./pages/about.svelte";
+  import Products from "./pages/products.svelte";
+  import ProductTemplate from "./pages/product-template.svelte";
+</script>
+
+<Router>
+  <nav class="navbar">
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/products">Products</Link>
+  </nav>
+
+  <div>
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/login" component={Login} />
+    <Route path="/products" component={Products} />
+    <Route path="/products/:id" component={ProductTemplate} />
+  </div>
+</Router>
---
+++ src/pages/home.svelte
@@ -0,0 +1,12 @@
+<script>
+  // routing
+  import { link } from "svelte-routing";
+  // components
+  import Hero from "../components/hero.svelte";
+  // featured products
+  // functions
+</script>
+
+<Hero>
+  <a href="/products" class="btn btn-primary btn-hero" use:link>shop now</a>
+</Hero>
